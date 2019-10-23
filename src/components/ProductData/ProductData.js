6ac09ddcad640bca059bde873038a3ba9f3e66a2
David <neodmy@hotmail.com>
2019-10-24 01:18:02 +0200

    Product details refactored in its own component

---
+++ src/components/ProductData/ProductData.js
@@ -0,0 +1,60 @@
+import React from 'react';
+
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Table from 'react-bootstrap/Table';
+
+import styles from './ProductData.module.css';
+
+const productData = (props) => {
+    const phoneDetails = () => {
+        let table = [];
+        const excludedProps = ['_id', 'name', 'imageFileName', 'description'];
+        for (const key in props.phone) {
+            if (props.phone.hasOwnProperty(key) && !excludedProps.includes(key)) {
+                const propUpperCase = key.charAt(0).toUpperCase() + key.slice(1);
+                table.push(
+                    <tr key={key}>
+                        <td>{propUpperCase}</td>
+                        <td>{props.phone[key]}</td>
+                    </tr>
+                );
+            }
+        }
+        return table;
+    }
+
+    return (
+        <Container className="text-light text-center" >
+            <Row className="row justify-content-center mt-5 mb-3 ">
+                <Col>
+                    <h1 className="display-5">{props.phone.name}</h1>
+                </Col>
+            </Row>
+            {props.edit}
+            <Row className="row justify-content-center">
+                <Col lg={4} className="my-auto">
+                    <img src={props.phone.imageFileName} alt="phone_image" className={styles.phone_img} />
+                </Col>
+                <Col lg={6}>
+                    <p>{props.phone.description}</p>
+                    <Table className="text-light">
+                        <thead>
+                            <tr>
+                                <th colSpan="2">
+                                    Tech Specs
+                            </th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {phoneDetails()}
+                        </tbody>
+                    </Table>
+                </Col>
+            </Row>
+        </Container >
+    );
+};
+
+export default productData;
--- src/containers/Products/Product/Product.js
+++ src/containers/Products/Product/Product.js
@@ -2,14 +2,12 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { Redirect } from 'react-router-dom';
 
-import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
-import Table from 'react-bootstrap/Table';
 import Button from 'react-bootstrap/Button';
 
-import styles from './Product.module.css';
 import CustomModal from '../../../components/CustomModal/CustomModal';
+import ProductData from '../../../components/ProductData/ProductData';
 import * as actions from '../../../store/actions';
 
 class Product extends Component {
@@ -24,23 +22,6 @@ class Product extends Component {
     constructor(props) {
         super(props);
         this.phone = props.phones.filter(phone => phone._id === props.selectedPhone)[0];
-    }
-
-    phoneDetails = () => {
-        let table = [];
-        const excludedProps = ['_id', 'name', 'imageFileName', 'description'];
-        for (const key in this.phone) {
-            if (this.phone.hasOwnProperty(key) && !excludedProps.includes(key)) {
-                const propUpperCase = key.charAt(0).toUpperCase() + key.slice(1);
-                table.push(
-                    <tr key={key}>
-                        <td>{propUpperCase}</td>
-                        <td>{this.phone[key]}</td>
-                    </tr>
-                );
-            }
-        }
-        return table;
     }
 
     onShowDeleteModalHandle = () => {
@@ -80,35 +61,11 @@ class Product extends Component {
                 />
             );
             content = (
-                <Container className="text-light text-center" >
+                <React.Fragment>
                     {deleteModal}
-                    <Row className="row justify-content-center mt-5 mb-3 ">
-                        <Col>
-                            <h1 className="display-5">{this.phone.name}</h1>
-                        </Col>
-                    </Row>
-                    {edit}
-                    <Row className="row justify-content-center">
-                        <Col lg={4} className="my-auto">
-                            <img src={this.phone.imageFileName} alt="phone_image" className={styles.phone_img} />
-                        </Col>
-                        <Col lg={6}>
-                            <p>{this.phone.description}</p>
-                            <Table className="text-light">
-                                <thead>
-                                    <tr>
-                                        <th colSpan="2">
-                                            Tech Specs
-                                        </th>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                    {this.phoneDetails()}
-                                </tbody>
-                            </Table>
-                        </Col>
-                    </Row>
-                </Container >
+                    <ProductData phone={this.phone} edit={edit} />
+                </React.Fragment>
+
             );
         }
         return content;
